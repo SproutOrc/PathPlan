678fb2c8d8144ea03e36820b5658b736b4dbd238
Atropos <atropos@promote.cache-dns.local>
2014-04-23 19:57:52 +0800

    Initial Commit

---
+++ PathPlan/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PathPlan
+//
+//  Created by Atropos on 14-4-23.
+//  Copyright (c) 2014年 Atropos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
